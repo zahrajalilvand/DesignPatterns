591fece5eb61335b95f0fcd66134c99ca386401d
zahrajalilvand <z.jalilehvand@sadad.co.ir>
2023-07-23 14:42:36 +0330

    Commit - Proxy sample

---
+++ Decorator/Decorator-DesignPattern/Proxy-DesignPattern/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy_DesignPattern;
+
+ISubject subject = new Proxy();
+subject.DoAction();
+
+Console.ReadKey();
---
+++ Decorator/Decorator-DesignPattern/Proxy-DesignPattern/Proxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_DesignPattern
+{
+    public class Proxy : ISubject
+    {
+        private RealSubject _realSubject;
+        public void DoAction()
+        {
+            if (_realSubject == null)
+            { 
+                _realSubject = new RealSubject();
+            }
+            
+            _realSubject.DoAction();
+        }
+    }
+}
